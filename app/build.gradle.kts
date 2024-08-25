675751444a3271984688db1d443e0cbc881a3329
krolchonok <fedosey8@gmail.com>
2024-08-25 18:14:57 +0500

    again again and again

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
-            shrinkResources = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
